1d42104e94358ae1e8d12482055c261dbfe517cb
774525000@qq.com <199441201qQ>
2019-12-29 01:47:28 +0800

    click login button

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8  -*-
+
+from app.train.train import Train
+from time import sleep
+
+
+def run(home_url, email, password, chaojiying_name, chaojiying_pass, chaojiying_app_id, pic_type):
+    Train.default(home_url, email, password, chaojiying_name, chaojiying_pass, chaojiying_app_id, pic_type)
+    sleep(100)
