b8c9b0b51250081a9317984c329b2907540284fc
774525000@qq.com <199441201qQ>
2019-12-28 21:10:33 +0800

    click login button

--- train/train/train.py
+++ train/train/train.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from train.chaojiyin.chaojiying import Chaojiying_Client
 import base64
 from time import sleep
-from random import random
+from random import randint
 
 
 def save_pic(url):
@@ -63,25 +63,52 @@ class Train:
 
     def click_img(self, arr):
         try:
-            img = self.driver.find_element_by_css_selector("#J-loginImg")
+            element = self.driver.find_element_by_css_selector("#J-loginImgArea")
             action = ActionChains(self.driver)
             for item in arr:
-                action.move_to_element_with_offset(img, int(item[0]), int(item[1])).click().perform()
-                sleep(random() + 0.5)
+                action.move_to_element_with_offset(element, int(item[0]) + randint(1, 5),
+                                                   int(item[1]) + randint(1, 5)).click().perform()
+                sleep(20)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def login(self):
+        try:
+            self.driver.find_element_by_css_selector('#J-login').click()
+        except Exception as e:
+            print(e)
+            raise e
+
+    def get_points(self, res):
+        try:
+            if res.get('pic_str'):
+                pic_str = res['pic_str']
+                return [item.split(',') for item in pic_str.split('|')]
         except Exception as e:
             print(e)
             raise e
 
     @classmethod
     def default(cls, url, email, password, chaojiying_name, chaojiying_pass, chaojiying_app_id, pic_type):
-        train = Train(url)
+        train = cls(url)
         train.enter_login()
+        # 获取验证码
         pic_src = train.get_pic()
+        # 保存图片
         save_pic(pic_src)
         # 打码
         chaojiying = Chaojiying_Client(chaojiying_name, chaojiying_pass, chaojiying_app_id)
-        im = open('./static/pic.png', 'rb').read()
-        pic_str = chaojiying.PostPic(im, pic_type)['pic_str']
-        arr = [item.split(',') for item in pic_str.split('|')]
+        with open('./static/pic.png', 'rb') as f:
+            im = f.read()
+        # 获取打码结果
+        res = chaojiying.PostPic(im, pic_type)
+        # 获取坐标
+        arr = train.get_points(res)
+        # 输入账号密码
         train.enter_account(email, password)
+        # 点击验证码
         train.click_img(arr)
+        # 登录
+        # train.login()
+
